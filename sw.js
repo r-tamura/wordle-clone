1c088a14a97421deee473aa7b632bf2d9b416421
r-tamura <r-tamura@users.noreply.github.com>
2022-01-30 12:49:44 +0000

    deploy: 88e734608b3f092194ddcc4fb82e61f144fc8ce3

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ self.addEventListener("install", function (event) {
   // Perform install steps
   event.waitUntil(
     caches.open(CACHE_NAME).then(function (cache) {
-      console.log("Opened cache");
+      console.log(`Opened cache: ${CACHE_NAME}`);
       return cache.addAll(urlsToCache);
     })
   );
@@ -14,11 +14,31 @@ self.addEventListener("install", function (event) {
 
 self.addEventListener("fetch", async (e) => {
   e.respondWith(
-    caches.match(e.request).then((response) => {
+    caches.match(e.request).then(async (response) => {
       if (response) {
         return response;
       }
-      return fetch(e.request);
+      response = await fetch(e.request);
+      if (!response || response.status !== 200 || response.type !== "basic") {
+        return response;
+      }
+
+      if ("URLPattern" in globalThis) {
+        if (
+          !new URLPattern("/*.json", globalThis.location).test(e.request.url)
+        ) {
+          return response;
+        }
+      } else {
+        if (!/.*\.json/.test(e.request.url)) {
+          return response;
+        }
+      }
+
+      const responseToCache = response.clone();
+      const cache = await caches.open(CACHE_NAME);
+      cache.put(e.request, responseToCache);
+      return response;
     })
   );
 });
